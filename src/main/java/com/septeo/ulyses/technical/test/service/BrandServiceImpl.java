0063ccb2dbf1ac4fd99629b3a8b0028bee16a1b7
yosri22 <yousribhiri@gmail.com>
2025-06-10 22:56:31 +0100

    feat(cache): add built-in Spring caching for Brand GET endpoints

--- src/main/java/com/septeo/ulyses/technical/test/service/BrandServiceImpl.java
+++ src/main/java/com/septeo/ulyses/technical/test/service/BrandServiceImpl.java
@@ -3,6 +3,8 @@ package com.septeo.ulyses.technical.test.service;
 import com.septeo.ulyses.technical.test.entity.Brand;
 import com.septeo.ulyses.technical.test.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +26,7 @@ public class BrandServiceImpl implements BrandService {
      * {@inheritDoc}
      */
     @Override
+    @Cacheable("brands")
     public List<Brand> getAllBrands() {
         return brandRepository.findAll();
     }
@@ -32,6 +35,7 @@ public class BrandServiceImpl implements BrandService {
      * {@inheritDoc}
      */
     @Override
+    @Cacheable(value = "brandById", key = "#id")
     public Optional<Brand> getBrandById(Long id) {
         return brandRepository.findById(id);
     }
@@ -40,6 +44,7 @@ public class BrandServiceImpl implements BrandService {
      * {@inheritDoc}
      */
     @Override
+    @CacheEvict(value = { "brands", "brandById" }, allEntries = true)
     public Brand saveBrand(Brand brand) {
         return brandRepository.save(brand);
     }
@@ -48,6 +53,7 @@ public class BrandServiceImpl implements BrandService {
      * {@inheritDoc}
      */
     @Override
+    @CacheEvict(value = { "brands", "brandById" }, allEntries = true)
     public void deleteBrand(Long id) {
         brandRepository.deleteById(id);
     }
